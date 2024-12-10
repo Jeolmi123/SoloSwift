21303892643ffd40ca25393a503e566d32cb79ca
Min <jmsj0308@naver.com>
2024-12-11 05:18:15 +0900

    [Add]: JustXractice File

---
+++ JustXractice/JustXractice/JustXracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustXracticeApp.swift
+//  JustXractice
+//
+//  Created by Min on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct JustXracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
